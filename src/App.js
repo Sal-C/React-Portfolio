bcd1c4e4aebf3399a1176e85a1687ab8d20014d7
Sal <salvadorchaveztrejo@gmail.com>
2020-06-04 07:51:37 -0700

    navbar component added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {
   Route
 } from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default function BasicExample() {
   return (
     <Router>
       <div>
+      <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+        <div className="navbar-fixed" id="navbar">
+            <nav className="nav-color" role="navigation">
+                <a id="logo-container" href="./index.html" className="brand-logo">Salvador Chavez</a>
+                <ul className="right hide-on-med-and-down">
+                    <li><a href="/bio">Home</a></li>
+                    <li><a href="/portfolio">Portfolio</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
