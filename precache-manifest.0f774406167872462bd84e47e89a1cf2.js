290918189d730a66be5ad045b005978e545b8c96
Sal <salvadorchaveztrejo@gmail.com>
2020-06-04 08:44:27 -0700

    Updates

---
+++ precache-manifest.0f774406167872462bd84e47e89a1cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569fb1b7f680025d3f8efeb7bb982cbc",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "84a70bb5b388f629fd1a",
+    "url": "/React-Portfolio/static/css/main.9d498ca3.chunk.css"
+  },
+  {
+    "revision": "b8c317ff5e7bdda4404d",
+    "url": "/React-Portfolio/static/js/2.a92cd763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.a92cd763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a70bb5b388f629fd1a",
+    "url": "/React-Portfolio/static/js/main.0cfd2cd1.chunk.js"
+  },
+  {
+    "revision": "7d80ea8ccf67eb39b90d",
+    "url": "/React-Portfolio/static/js/runtime-main.9578ddf3.js"
+  },
+  {
+    "revision": "7d22f06ee0f8679e889bb8b72ea85742",
+    "url": "/React-Portfolio/static/media/OpenMine.7d22f06e.ttf"
+  }
+]);
